d7bf2ca7204fbb337ad6bb6ae4d09df7031930b2
Martin <gomez.martin1158@gmail.com>
2022-06-08 20:36:20 -0300

    Se dejo la app mas canchera

--- ANDROID/Cultibot/app/src/main/java/com/example/dashboardview/MainActivity.java
+++ ANDROID/Cultibot/app/src/main/java/com/example/dashboardview/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.dashboardview;
 import static java.lang.Math.abs;
 
 import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,16 +21,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private final int UPDATING_STATUS = 1000;
     private final int UPDATED_STATUS = 1001;
     private final int SHAKE_THRESHOLD = 10;
-    private final int FIVE_SECONDS = 5000;
+    private final int SECONDS = 3000;
 
     private boolean semaphore = false;
 
     private SensorManager mSensorManager;
 
     private TextView statusText;
+
     private TextView temperatureText;
     private TextView lightText;
     private TextView humidityText;
+
+    private TextView temperatureValue;
+    private TextView lightValue;
+    private TextView humidityValue;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -43,9 +48,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         // Defino los txt para representar los datos de los sensores
         statusText      = (TextView) findViewById(R.id.statusText);
+
         temperatureText = (TextView) findViewById(R.id.temperatureText);
         lightText       = (TextView) findViewById(R.id.lightText);
         humidityText    = (TextView) findViewById(R.id.humidityText);
+
+        temperatureValue = (TextView) findViewById(R.id.temperatureValue);
+        lightValue       = (TextView) findViewById(R.id.lightValue);
+        humidityValue    = (TextView) findViewById(R.id.humidityValue);
+
 
         // Accedemos al servicio de sensores
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -60,10 +71,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         });
 
+        temperatureText.setText("Temperatura");
+        humidityText.setText("Humedad");
+        lightText.setText("Luminosidad");
+
         // Hardcodeo de sensores
-        temperatureText.setText("Temperatura: 22°C");
-        humidityText.setText("Humedad 75%");
-        lightText.setText("Luminosidad 55%");
+        temperatureValue.setTypeface(temperatureValue.getTypeface(), Typeface.BOLD);
+        humidityValue.setTypeface(humidityValue.getTypeface(), Typeface.BOLD);
+        lightValue.setTypeface(lightValue.getTypeface(), Typeface.BOLD);
+
+        temperatureValue.setText("22°");
+        humidityValue.setText("75%");
+        lightValue.setText("55%");
 
         updateStatus(UPDATED_STATUS);
     }
@@ -91,7 +110,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                  ThreadAsyncTask hilo =  new ThreadAsyncTask();
                  hilo.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
                 semaphore = true;
-                Log.i("onCreate",  "Se creo un nuevo hilo");
                 break;
             case UPDATED_STATUS:
                 statusText.setText("Actualizado");
@@ -142,9 +160,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         @Override
         protected Integer doInBackground(Integer... integers) {
             try {
-                Log.i("onCreate", "Hilo pre siesta");
-                Thread.sleep(FIVE_SECONDS);
-                Log.i("onCreate", "Hilo post siesta");
+                Thread.sleep(SECONDS);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -154,7 +170,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         protected void onPostExecute(Integer result){
             updateStatus(UPDATED_STATUS);
             semaphore = false;
-            Log.i("onCreate",  "Se termino el hilo");
         }
     }
 }
